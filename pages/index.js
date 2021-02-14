aa9c7861f75476e6de0236b83909175ee74094e3
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-02-13 21:45:23 -0300

    Fim da aula 10

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,37 @@
 import { Button } from '../src/components/commons/Button';
 import Footer from '../src/components/commons/Footer';
+import { Box } from '../src/components/layout/Box'
 import { Grid } from '../src/components/layout/Grid'
 import Menu from '../src/components/commons/Menu';
 import Text from '../src/components/foundation/Text';
 
 export default function Home() {
   return (
-    <div style={{
-      flex: '1',
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
+    <Box
+      flex='1'
+      display='flex'
+      flexWrap='wrap'
+      flexDirection='column'
+      justifyContent='space-between'
+      backgroundImage='url(/images/bubbles.svg)'
+      backgroundRepeat='no-repeat'
+      backgroundPosition='bottom right'
+    >
       <Menu />
 
-      <Grid.Container>
+      <Grid.Container
+        marginTop={{
+          xs: '32px',
+          md: '75px',
+        }}>
         <Grid.Row>
           <Grid.Col
-            offset={1}
             value={{ xs: 12, md: 5 }}
+            offset={{ xs: 0, md: 1 }}
+            display='flex'
+            alignItems='flex-start'
+            justifyContent='center'
+            flexDirection='column'
           >
             <div>
               <Text
@@ -70,6 +82,6 @@ export default function Home() {
       </Grid.Container>
 
       <Footer />
-    </div>
+    </Box>
   )
 }
