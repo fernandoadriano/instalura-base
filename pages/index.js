647e682020f779974dd870d091bf391a7dac63cf
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-03 18:50:06 -0300

    Fim do teste de husky

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 import Button from '../src/components/commons/Button';
 import Footer from '../src/components/commons/Footer';
