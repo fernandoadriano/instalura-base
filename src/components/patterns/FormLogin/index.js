57604de78f84bd4acc1efa56b67f1e4299d0eaea
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-28 10:18:54 -0300

    feat: fim da parte 1 da aula 2

---
+++ src/components/patterns/FormLogin/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Button from 'src/components/commons/Button';
+import TextField from 'src/components/forms/TextField';
+import { useForm } from 'src/infra/hooks/forms/useForm';
+import { loginService } from 'src/services/login/loginService';
+
+export default function LoginForm() {
+  const router = useRouter();
+  const initialValues = {
+    usuario: '',
+    senha: '',
+  };
+  const form = useForm({
+    initialValues,
+    onSubmit: (values) => {
+      loginService.login({
+        username: values.usuario, // 'omariosouto'
+        password: values.senha, // 'senhasegura'
+      })
+        .then(() => {
+          router.push('/app/profile');
+        });
+    },
+  });
+
+  return (
+    <form id="formCadastro" onSubmit={form.handleSubmit}>
+      <TextField
+        placeholder="Usuário"
+        name="usuario"
+        value={form.values.usuario}
+        onChange={form.handleChange}
+      />
+      <TextField
+        placeholder="Senha"
+        name="senha"
+        type="password"
+        value={form.values.senha}
+        onChange={form.handleChange}
+      />
+
+      <Button
+        type="submit"
+        variant="primary.main"
+        margin={{
+          xs: '0 auto',
+          md: 'initial',
+        }}
+        fullWidth
+      >
+        Entrar
+      </Button>
+    </form>
+  );
+}
