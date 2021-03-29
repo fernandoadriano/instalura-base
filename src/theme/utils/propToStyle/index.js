fb6e18b258a31edae49514b2ee8dfb9ff8e6e030
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-29 09:22:38 -0300

    fix: ajuste estético no propToStyle

--- src/theme/utils/propToStyle/index.js
+++ src/theme/utils/propToStyle/index.js
@@ -6,25 +6,18 @@ export default function propToStyle(propName) {
     const propValue = props[propName];
 
     if (typeof propValue === 'object') {
-      return css`
-            ${breakpointsMedia({
-    ...(propValue.xs && {
-      xs: { [propName]: propValue.xs },
-    }),
-    ...(propValue.sm && {
-      sm: { [propName]: propValue.sm },
-    }),
-    ...(propValue.md && {
-      md: { [propName]: propValue.md },
-    }),
-    ...(propValue.lg && {
-      lg: { [propName]: propValue.lg },
-    }),
-    ...(propValue.xl && {
-      xl: { [propName]: propValue.xl },
-    }),
-  })}
-      `;
+      return (css`
+        ${breakpointsMedia(
+          {
+            ...(propValue.xs && { xs: { [propName]: propValue.xs } }),
+            ...(propValue.sm && { sm: { [propName]: propValue.sm } }),
+            ...(propValue.md && { md: { [propName]: propValue.md } }),
+            ...(propValue.lg && { lg: { [propName]: propValue.lg } }),
+            ...(propValue.xl && { xl: { [propName]: propValue.xl } }),
+          },
+        )
+        }
+      `);
     }
 
     return {
