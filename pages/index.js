2f3dd2b2ce4a81a201460ba62a25df5713c1c28e
Fernando Adriano Machado <fernando.adriano@newspace.com.br>
2021-03-29 22:13:57 -0300

    feat: fim da aula 09

--- pages/index.js
+++ pages/index.js
@@ -97,20 +97,3 @@ export default websitePageHOC(HomeScreen, {
     },
   },
 });
-
-// export default function Home() {
-//   return (
-//     <WebsitePageWrapper
-//       seoProps={{
-//         headTitle: 'Home',
-//       }}
-//       pageBoxProps={{
-//         backgroundImage: 'url(/images/bubbles.svg)',
-//         backgroundRepeat: 'no-repeat',
-//         backgroundPosition: 'bottom right',
-//       }}
-//     >
-//       <HomeScreen />
-//     </WebsitePageWrapper>
-//   );
-// }
